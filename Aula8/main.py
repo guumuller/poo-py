f98c24e140072c84de41b4a4f6bc481d39f87c51
Gustavo Müller Leonini Machado <gustavomuller160305@gmail.com>
2024-10-03 11:24:43 -0300

    herança animais

--- Aula8/main.py
+++ Aula8/main.py
@@ -1,34 +1,6 @@
-class Animal:
-  def __init__(self, nome, idade):
-    self.nome = nome
-    self.idade = idade
-
-  def emitir_som(self):
-    pass  # Implementar em cada classe filha
-
-class Cachorro(Animal):
-  def __init__(self, nome, idade, raca):
-    super().__init__(nome, idade)
-    self.raca = raca
-
-  def emitir_som(self):
-    print(f"{self.nome} está latindo!")
-
-class Gato(Animal):
-  def __init__(self, nome, idade, cor):
-    super().__init__(nome, idade)
-    self.cor = cor
-
-  def emitir_som(self):
-    print(f"{self.nome} está miando!")
-
-class Passaro(Animal):
-  def __init__(self, nome, idade, especie):
-    super().__init__(nome, idade)
-    self.especie = especie
-
-  def emitir_som(self):
-    print(f"{self.nome} está cantando!")
+from cachorro import Cachorro
+from gato import Gato
+from passaro import Passaro
 
 # Criando objetos e utilizando métodos
 cachorro = Cachorro("Rex", 3, "Labrador")
